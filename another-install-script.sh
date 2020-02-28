b6a2b6347c58990a55018f6c425cb12957021b50
Antoine <antoine.bou13@gmail.com>
2020-02-28 15:58:56 -0500

    backup commit

---
+++ another-install-script.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# @file install.sh
+# @brief file containing the utils  for the project and other
+
+source "$(dirname "${BASH_SOURCE[0]}")/src/menu.sh"
+
+# Load the user defined parameters
+while [[ $# > 0 ]]; do
+    case "$1" in
+        --help|*)
+            cat docs/help.txt
+        exit 1
+        ;;
+        esac
+    shift
+done
+
+main_setup_menu
