658a2b7469b826f17245c05b035b1e329de27ef1
Antoine Boucher <antoine.bou13@gmail.com>
2020-03-09 11:15:29 -0400

    add opentogethertube

---
+++ src/images/opentogethertube/opentogethertube.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# @file opentogethertube.sh
+# @brief to install docker opentogethertube
+
+# @description create docker opentogethertube
+# https://github.com/antoinebou13/opentogethertube/tree/docker
+# @noargs
+# @exitcode 0 If successfull.
+# @exitcode 1 On failure
+create_docker_olaris() {
+   docker-compose -f "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)/docker-compose.yml" up -d
+
+   echo "ctrl+click to open in browser"
+   echo "$(get_current_ip):6666"
+   return 0
+}
