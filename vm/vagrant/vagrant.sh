1ffc0f0ca2e635292c2c28c8375c0d4b1d6b3200
Antoine Boucher <antoine.bou13@gmail.com>
2020-03-10 16:46:49 -0400

    update vagrant script

---
+++ vm/vagrant/vagrant.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# @file vagrant.sh
+# @brief vagrant utils functions
+
+# @description start_ubuntu
+#
+# @args $1 vagrant ubuntu code name xenial64/eaon64
+# @args $2 branch name
+# @exitcode 0 If successfull.
+# @exitcode 1 On failure
+vagrant_ubuntu() {
+    pushd "$1" || exit
+    vagrant up && vagrant ssh <<EOF
+git clone https://github.com/antoinebou13/another-install-script.git
+cd another-install-script || exit
+git checkout $2
+EOF
+    vagrant ssh
+    popd || exit
+}
+
+# @description destroy vagrant
+#
+# @args $1 vagrantfile location
+# @exitcode 0 If successfull.
+# @exitcode 1 On failure
+vagrant_destroy() {
+    pushd "$1" || exit
+    vagrant destroy -f
+    popd || exit
+}
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    create)
+        vagrant_ubuntu "$2" "$3"
+        exit 1
+        ;;
+    destroy)
+        vagrant_destroy "$2"
+        exit 1
+        ;;
+    esac
+    shift
+done
